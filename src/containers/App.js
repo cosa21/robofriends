6f8ac8203908aefd3bf246e5146f9e1f60a04516
Lazar Cosovic <lazarcosovic21@gmail.com>
2020-11-08 23:44:22 +0100

    initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+class App extends React.Component {
+
+    constructor(){
+        super();
+        this.state={
+            robots:[],
+            searchField:''
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users').then(response=>response.json()).then(users=>this.setState({robots:users}));
+    }
+
+    searchChange = (event)=>{
+        this.setState({searchField:event.target.value})
+    }
+    render() {
+        const {robots,searchField}=this.state;
+        const filteredRobots=robots.filter(robot=>{
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
+        })
+        if(robots.length===0){
+            return <h1>Loading...</h1>
+        }else{
+            return (
+                <div className="tc">
+                    <h1 className='f1'>Robofriends</h1>
+                    <SearchBox searchChange={this.searchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            );
+        }
+        
+    }
+}
+
+export default App;
